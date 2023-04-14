1d615dc23991260e0db0386078218d7edb558c87
Aleksey <11_alx_11@ro.ru>
2023-04-14 19:19:55 +0300

    working on labrab2

--- labrab2/src/LocalHttpServer.java
+++ labrab2/src/LocalHttpServer.java
@@ -5,11 +5,9 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class LocalHttpServer {
-    public static final int RESPONSE_LENGTH = 400;
     private final int port;
     private final int pool_size;
     private boolean stopped;
@@ -24,43 +22,48 @@ public class LocalHttpServer {
             var server_socket = new ServerSocket(port);
             var pool = Executors.newFixedThreadPool(this.pool_size);
             while (!stopped) {
-                var socket = server_socket.accept();
+                Socket socket = server_socket.accept();
                 System.out.println("Socket accepted");
-                pool.submit(() -> processSocket(socket));
+                pool.execute(() -> {
+                    try (socket;
+                         var input_stream = new DataInputStream(socket.getInputStream());
+                         var output_stream = new DataOutputStream(socket.getOutputStream())
+                    ) {
+                        System.out.println("Request: " + new String(input_stream.readAllBytes()));
+
+                        Thread.sleep(1000);
+
+                        var body = Files.readAllBytes(Path.of("resources", "index.html"));
+                        var headers = """
+                            HTTP/1.1 200 OK
+                            content-type: text/html
+                            content-length: %s
+                            """.formatted(body.length).getBytes();
+                        var line_separator = System.lineSeparator().getBytes();
+
+                        output_stream.write(headers);
+                        output_stream.write(line_separator);
+                        output_stream.write(body);
+                        output_stream.write(line_separator);
+                        output_stream.write(line_separator);
+                    }
+                    // TODO: log error msg
+                    catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    finally {
+                        try {
+                            socket.close();
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                });
             }
         } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void processSocket(Socket socket) {
-        try (socket;
-             var input_stream = new DataInputStream(socket.getInputStream());
-             var output_stream = new DataOutputStream(socket.getOutputStream())
-        ) {
-            System.out.println("Request: " + new String(input_stream.readNBytes(RESPONSE_LENGTH)));
-
-            Thread.sleep(1000);
-
-            var body = Files.readAllBytes(Path.of("resources", "index.html"));
-            var headers = """
-                    HTTP/1.1 200 OK
-                    content-type: text/html
-                    content-length: %s
-                    """.formatted(body.length).getBytes();
-            var line_separator = System.lineSeparator().getBytes();
-
-            output_stream.write(headers);
-            output_stream.write(line_separator);
-            output_stream.write(body);
-            output_stream.write(line_separator);
-            output_stream.write(line_separator);
-        }
-        // TODO: log error msg
-        catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
