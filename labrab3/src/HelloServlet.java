171ab33acef6a0dda1f73c03cbc3966434dcc411
Aleksey <11_alx_11@ro.ru>
2023-06-03 05:21:51 +0300

    working on labrabs

--- labrab3/src/HelloServlet.java
+++ labrab3/src/HelloServlet.java
@@ -3,17 +3,25 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.util.Enumeration;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 
+@WebServlet("/HelloServlet")
 public class HelloServlet extends HttpServlet {
 
     @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String imagePath = "test.jpg";
+        String imagePath = "\"C:\\Users\\marin\\Downloads\\apache-tomcat-10.1.7-windows-x64\\apache-tomcat-10.1.7\\webapps\\ROOT\\test.jpg\"";
         File imageFile = new File(imagePath);
         if (imageFile.exists()) {
             String contentType = Files.probeContentType(imageFile.toPath());
--- labrab4/HelloServlet.java
+++ labrab4/HelloServlet.java
@@ -1,12 +1,13 @@
-import java.io.File;
+
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.nio.file.Files;
+import java.util.Date;
 import java.util.Enumeration;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
 
 public class HelloServlet extends HttpServlet {
 
@@ -31,7 +32,6 @@ public class HelloServlet extends HttpServlet {
             out.println("<h2>Добро пожаловать снова!</h2>");
             out.println("Дата последнего посещения: " + lastVisitDate + "<br>");
             out.println("Браузер: " + userAgent);
-            out.println("</body></html>");
         } else {
             // Кука не существует, пользователь уникальный
             String currentDate = new Date().toString();
@@ -45,21 +45,24 @@ public class HelloServlet extends HttpServlet {
 
             // Запись данных в текстовый файл
             String logEntry = "Дата посещения: " + currentDate + ", IP адрес: " + ipAddress + ", Браузер: " + userAgent;
-            writeToFile(logEntry);
-
+            try {
+				writeToFile(logEntry);
+			}
+			catch (IOException e) {
+				out.println("<p>Ошибка логирования</p>");
+			}
             // Создание куки
             Cookie newCookie = new Cookie("uniqueVisitor", currentDate);
             newCookie.setMaxAge(24 * 60 * 60); // Установка срока жизни куки на 24 часа
             response.addCookie(newCookie);
-
-            out.println("</body></html>");
+        }
 
             // Вывод атрибутов ServletContext, HttpSession и HttpServletRequest
             out.println("<h2>Атрибуты:</h2>");
 
             // Получение атрибутов ServletContext
-            javax.servlet.ServletContext servletContext = getServletContext();
-            java.util.Enumeration<String> servletContextAttributes = servletContext.getAttributeNames();
+            ServletContext servletContext = getServletContext();
+            Enumeration<String> servletContextAttributes = servletContext.getAttributeNames();
             while (servletContextAttributes.hasMoreElements()) {
                 String attributeName = servletContextAttributes.nextElement();
                 Object attributeValue = servletContext.getAttribute(attributeName);
@@ -67,8 +70,8 @@ public class HelloServlet extends HttpServlet {
             }
 
             // Получение атрибутов HttpSession
-            javax.servlet.http.HttpSession session = request.getSession();
-            java.util.Enumeration<String> sessionAttributes = session.getAttributeNames();
+            HttpSession session = request.getSession();
+            Enumeration<String> sessionAttributes = session.getAttributeNames();
             while (sessionAttributes.hasMoreElements()) {
                 String attributeName = sessionAttributes.nextElement();
                 Object attributeValue = session.getAttribute(attributeName);
@@ -76,13 +79,15 @@ public class HelloServlet extends HttpServlet {
             }
 
             // Получение атрибутов HttpServletRequest
-            java.util.Enumeration<String> requestAttributes = request.getAttributeNames();
+            Enumeration<String> requestAttributes = request.getAttributeNames();
             while (requestAttributes.hasMoreElements()) {
                 String attributeName = requestAttributes.nextElement();
                 Object attributeValue = request.getAttribute(attributeName);
                 out.println("HttpServletRequest attribute: " + attributeName + " = " + attributeValue + "<br>");
             }
-        }
+
+
+        out.println("</body></html>");
     }
 
     private Cookie getCookieByName(Cookie[] cookies, String name) {
@@ -96,8 +101,8 @@ public class HelloServlet extends HttpServlet {
         return null;
     }
 
-    private void writeToFile(String logEntry) {
-        FileWriter writer = new FileWriter("log.txt", true);
+    private void writeToFile(String logEntry) throws IOException {
+        FileWriter writer = new FileWriter("/log.txt", true);
         writer.write(logEntry + "\n");
         writer.close();
     }
